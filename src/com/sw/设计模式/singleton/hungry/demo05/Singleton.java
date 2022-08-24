9ac68383d64a2e1fc44a36736388afc8114c4f2f
wanghao <281463547@qq.com>
2022-08-24 22:41:55 +0800

    单例模式-懒汉式（静态内部类）

---
+++ src/com/sw/设计模式/singleton/hungry/demo05/Singleton.java
@@ -0,0 +1,21 @@
+package com.sw.设计模式.singleton.hungry.demo05;
+
+/**
+ * @author Wang Hao
+ * @date 2022/8/24 22:33
+ * @description 懒汉式（静态内部类）
+ */
+public class Singleton {
+
+    private Singleton() {
+
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+}
