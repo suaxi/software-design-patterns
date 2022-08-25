17ba527db8cd985caed1e35d03f1118697b457ef
wanghao <281463547@qq.com>
2022-08-25 21:48:18 +0800

    枚举方式

---
+++ src/com/sw/设计模式/singleton/enums/Client.java
@@ -0,0 +1,15 @@
+package com.sw.设计模式.singleton.enums;
+
+/**
+ * @author Wang Hao
+ * @date 2022/8/25 21:46
+ */
+public class Client {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.INSTANCE;
+        Singleton instance1 = Singleton.INSTANCE;
+
+        //true
+        System.out.println(instance == instance1);
+    }
+}
---
+++ src/com/sw/设计模式/singleton/enums/Singleton.java
@@ -0,0 +1,10 @@
+package com.sw.设计模式.singleton.enums;
+
+/**
+ * @author Wang Hao
+ * @date 2022/8/25 21:45
+ * @description 枚举实现方式
+ */
+public enum Singleton {
+    INSTANCE;
+}
